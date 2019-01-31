5d5b55cd9f4cdc50335dd1e4c420c08001cbcf00
Yunato <yunato525@gmail.com>
2019-01-31 16:57:45 +0900

    Create RecordListFragment

---
+++ app/src/main/java/io/github/yunato/mycashbook/ui/adapter/MyRecordRecyclerViewAdapter.kt
@@ -0,0 +1,49 @@
+package io.github.yunato.mycashbook.ui.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+import io.github.yunato.mycashbook.R
+import io.github.yunato.mycashbook.ui.fragment.RecordListFragment.OnSelectListener
+import io.github.yunato.mycashbook.ui.fragment.dummy.DummyContent.DummyItem
+
+class MyRecordRecyclerViewAdapter(private val mValues: List<DummyItem>, private val mListener: OnSelectListener?) : RecyclerView.Adapter<MyRecordRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.fragment_record_item_list, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.mItem = mValues[position]
+        holder.mIdView.text = mValues[position].id
+        holder.mContentView.text = mValues[position].content
+
+        holder.mView.setOnClickListener {
+            mListener?.onSelect(mValues[position])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return mValues.size
+    }
+
+    inner class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
+        val mIdView: TextView
+        val mContentView: TextView
+        var mItem: DummyItem? = null
+
+        init {
+            mIdView = mView.findViewById(R.id.id) as TextView
+            mContentView = mView.findViewById(R.id.content) as TextView
+        }
+
+        override fun toString(): String {
+            return super.toString() + " '" + mContentView.text + "'"
+        }
+    }
+}
---
+++ app/src/main/java/io/github/yunato/mycashbook/ui/fragment/RecordListFragment.kt
@@ -0,0 +1,63 @@
+package io.github.yunato.mycashbook.ui.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import io.github.yunato.mycashbook.R
+import io.github.yunato.mycashbook.ui.adapter.MyRecordRecyclerViewAdapter
+import io.github.yunato.mycashbook.ui.fragment.dummy.DummyContent
+import io.github.yunato.mycashbook.ui.fragment.dummy.DummyContent.DummyItem
+
+class RecordListFragment : Fragment() {
+
+    private var mListener: OnSelectListener? = null
+
+    companion object {
+
+        fun newInstance(): RecordListFragment {
+            return RecordListFragment()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater?.inflate(R.layout.fragment_record_item_list, container, false)
+
+        if (view is RecyclerView) {
+            val context = view.getContext()
+            val recyclerView = view
+            recyclerView.layoutManager = LinearLayoutManager(context)
+            recyclerView.adapter = MyRecordRecyclerViewAdapter(DummyContent.ITEMS, mListener)
+        }
+        return view
+    }
+
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is OnSelectListener) {
+            mListener = context as OnSelectListener?
+        } else {
+            throw RuntimeException(context!!.toString() + " must implement OnListFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        mListener = null
+    }
+
+    interface OnSelectListener {
+        fun onSelect(item: DummyItem)
+    }
+}

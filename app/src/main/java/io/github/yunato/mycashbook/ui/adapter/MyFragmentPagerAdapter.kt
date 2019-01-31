fd2db76fd936dae5ba4dd6a76d6c50857221a850
Yunato <yunato525@gmail.com>
2019-01-31 17:07:49 +0900

    Create MyFragmentPagerAdapter

---
+++ app/src/main/java/io/github/yunato/mycashbook/ui/adapter/MyFragmentPagerAdapter.kt
@@ -0,0 +1,24 @@
+package io.github.yunato.mycashbook.ui.adapter
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import io.github.yunato.mycashbook.ui.fragment.InputRecordFragment
+import io.github.yunato.mycashbook.ui.fragment.RecordListFragment
+
+class MyFragmentPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    val tabTitles: Array<CharSequence> = arrayOf("タブ1", "タブ2")
+
+    override fun getPageTitle(position: Int): CharSequence = tabTitles[position]
+
+    override fun getItem(position: Int): Fragment? {
+        return when (position){
+            0 -> InputRecordFragment.newInstance()
+            1 -> RecordListFragment.newInstance()
+            else -> null
+        }
+    }
+
+    override fun getCount(): Int = tabTitles.size
+}

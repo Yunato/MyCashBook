82e76d64cef113431cc800fdf2d130cfa994ffe3
Yunato <yunato525@gmail.com>
2019-02-01 09:16:10 +0900

    Create UI on MoneyActivity

---
+++ app/src/main/java/io/github/yunato/mycashbook/ui/activity/MoneyActivity.kt
@@ -0,0 +1,13 @@
+package io.github.yunato.mycashbook.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.github.yunato.mycashbook.R
+
+class MoneyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_money)
+    }
+}

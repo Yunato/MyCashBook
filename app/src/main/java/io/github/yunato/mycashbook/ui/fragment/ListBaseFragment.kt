8acab822ca5e0cbc5d1cd6f50d413bd920649155
Yunato <yunato525@gmail.com>
2019-02-01 11:06:44 +0900

    Create ListBaseFragment

---
+++ app/src/main/java/io/github/yunato/mycashbook/ui/fragment/ListBaseFragment.kt
@@ -0,0 +1,37 @@
+package io.github.yunato.mycashbook.ui.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import io.github.yunato.mycashbook.R
+
+class ListBaseFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_list_base, container, false)
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+    companion object {
+
+        fun newInstance(): ListBaseFragment {
+            return ListBaseFragment()
+        }
+    }
+}
